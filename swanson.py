aff87e04923827a52feaff30dc80d3b1cb0b8f68
Pessimist101 <issythegirl@gmail.com>
2020-05-09 21:44:48 +0100

    This should work!

---
+++ swanson.py
@@ -0,0 +1,28 @@
+import aiohttp
+import discord
+from discord.ext import commands
+import json
+
+
+class Swanson(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Ron Swanson cog online')
+
+    # Commands
+    @commands.command()
+    async def swanson(self, ctx):
+        async with aiohttp.ClientSession() as session:
+            data = await session.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+            quote = data[2:-2] # slice extraneous characters
+        embed = discord.Embed(description=quote)
+        # await ctx.message.delete()
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Swanson(client))
